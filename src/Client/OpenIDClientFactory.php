d68408d29d7b96ca466da20ab803e14d477ab14f
Matthias <m@tth.es>
2020-07-10 16:10:26 +0200

    api tryout

--- src/Client/OpenIDClientFactory.php
+++ src/Client/OpenIDClientFactory.php
@@ -10,6 +10,7 @@ namespace Phore\JWT\Client;
 
 
 use Phore\Cache\CacheItemPool;
+use Phore\Tests\OpenIdAuthFlowTest;
 
 class OpenIDClientFactory
 {
@@ -33,15 +34,42 @@ class OpenIDClientFactory
     }
 
 
-    public function getOpenIdClient(string $openIdHost, string $discoveryPath="/.well-known/openid-configuration.json") : OpenIDClient
+
+    protected function getDiscoveryConfig()
     {
         $cache = $this->cacheItemPool->getItem("openid_{$openIdHost}_{$discoveryPath}");
 
         $config = $cache->load(function() use ($openIdHost, $discoveryPath) {
             return phore_http_request("{$openIdHost}{$discoveryPath}")->send()->getBodyJson();
         });
+        return $config;
+    }
 
+    /**
+     * Return OpenID Client for webservice token verification
+     *
+     * @param string $openIdHost
+     * @param string $discoveryPath
+     * @return OpenIDClient
+     */
+    public function getOpenIdClient(string $openIdHost, string $discoveryPath="/.well-known/openid-configuration.json") : OpenIDClient
+    {
+        $config = $this->getDiscoveryConfig();
         return new OpenIDClient($config, $this->cacheItemPool);
+    }
+
+
+    /**
+     * Return OpenID Client for Resource Owner (Frontend) usage
+     *
+     * @param string $openIdHost
+     * @param string $discoveryPath
+     * @return OpenIDAuthFlowClient
+     */
+    public function getOpenIdAuthFlowClient(string $openIdHost, string $discoveryPath="/.well-known/openid-configuration.json") : OpenIDAuthFlowClient
+    {
+        $config = $this->getDiscoveryConfig();
+        return new OpenIDAuthFlowClient($config, $this->cacheItemPool);
     }
 
 }
---
+++ tests/OpenIdClientTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthias
+ * Date: 10.07.20
+ * Time: 15:58
+ */
+
+namespace Phore\Tests;
+
+
+use Phore\JWT\Client\OpenIDClientFactory;
+use PHPUnit\Framework\TestCase;
+
+class OpenIdClientTest extends TestCase
+{
+
+
+    public function testGetClientToken()
+    {
+        $factory = new OpenIDClientFactory();
+        $client = $factory->getOpenIdClient("http://localhost");
+        $client->setClientId("clientId", "clientSecret");
+
+        $token = $client->getClientToken();
+
+
+    }
+
+
+    public function testGetValidatedToken()
+    {
+        $factory = new OpenIDClientFactory();
+        $client = $factory->getOpenIdClient("http://localhost");
+        $client->setClientId("clientId", "clientSecret");
+
+        $token = $client->getValidatedToken();
+
+
+    }
+
+}
